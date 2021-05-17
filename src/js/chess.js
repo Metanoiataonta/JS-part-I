fdeb0ac0dbe9ac1996bfef160eb3f6128b394071
Metanoiataonta <73580892+Metanoiataonta@users.noreply.github.com>
2021-05-17 23:24:10 +0700

    Lesson 5 In progress

--- src/js/chess.js
+++ src/js/chess.js
@@ -1,25 +1,3 @@
-var formArea = document.querySelectorAll('.form'),
-    resultArea = document.querySelectorAll('.result'),
-    primeNumberClearArray = ["topNumber"];
-
-
-// used in clearInput function
-
-function resultPlaceholder() {
-    for (var result of resultArea) {
-        result.innerHTML = 'Result';
-    }
-}
-
-function clearInput(clearArray, index) {
-    for (var clear of clearArray) {
-        document.getElementById(clear).value = '';
-
-    }
-    resultArea[index].innerHTML = 'Result';
-}
-
-resultPlaceholder();
 var board = {
     squares: [],
     element: {}
